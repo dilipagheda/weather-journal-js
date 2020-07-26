52d62b391ffe064614dbee0e8b3f68d274441ebb
Dilip Agheda <dilip_agheda@yahoo.com>
2020-07-26 15:10:52 +1000

    Completed the requirements of the project

--- server.js
+++ server.js
@@ -2,8 +2,13 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors')
+const PORT = 3000;
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -11,34 +16,36 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
+app.listen(PORT, () => console.log(`Weather Journal Server is listening at http://localhost:${PORT}`));
 
+// GET route
+app.get("/api/entry", function (request, response) {
+    response.json(projectData);
+});
 
-//////////////////
+//POST route
+app.post('/api/entry', function (request, response) {
+    const userData = request.body;
+    if (userData && userData.temperature && userData.date && userData.feeling) {
+        projectData.temperature = userData.temperature;
+        const date = new Date(userData.date * 1000);
+        const dateTimeFormat = new Intl.DateTimeFormat('en', { year: 'numeric', month: 'short', day: '2-digit' })
+        const [{ value: month }, , { value: day }, , { value: year }] = dateTimeFormat.formatToParts(date)
+        projectData.date = `${day} ${month} ${year}`;
+        projectData.feeling = userData.feeling;
+        projectData.city = userData.city;
+        projectData.country = userData.country;
 
-// Setup empty JS object to act as endpoint for all routes
-// Express to run server and routes
-
-// Start up an instance of app
-
-/* Dependencies */
-/* Middleware*/
-
-//Here we are configuring express to use body-parser as middle-ware.
-// Cors for cross origin allowance
-
-// Initialize the main project folder
-
-// Spin up the server
-// Callback to debug
-
-// Initialize all route with a callback function
-
-// Callback function to complete GET '/all'
-
-// Post Route
+        response.status(201).end();
+    }
+    else {
+        response.status(400).json({ error: "bad data" });
+    }
+});
--- website/app.js
+++ website/app.js
@@ -1,21 +1,158 @@
 /* Global Variables */
 
-// Create a new date instance dynamically with JS
-let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
-
-////////////
-
-
 // Personal API Key for OpenWeatherMap API
+const API_KEY = '<INSERT_YOUR_API_KEY_HERE>';
 
-// Event listener to add function to existing HTML DOM element
-
-/* Function called by event listener */
+//URLs
+const BASE_URL = 'https://api.openweathermap.org/data/2.5/forecast?';
+const POST_ENTRY_ENDPOINT = 'http://localhost:3000/api/entry';
+const GET_ENTRY_ENDPOINT = 'http://localhost:3000/api/entry';
 
 /* Function to GET Web API Data*/
+function getWeatherData(zipCode, countryCode = 'US') {
+    //construct the full url
+    const url = `${BASE_URL}zip=${zipCode},${countryCode}&units=metric&appid=${API_KEY}`;
+
+    //make a fetch call
+    return fetch(url)
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            console.log(data)
+            if (data.cod !== "200") {
+                updateUIWithAPIError(data.message);
+                return;
+            }
+            return data;
+        })
+}
+
+// Event listener to add function to existing HTML DOM element
+document.getElementById('generate').addEventListener('click', clickHandler);
+
+/* Function called by event listener */
+function clickHandler() {
+    //reset UI
+    resetUI();
+    //get the country code
+    const countryCode = document.getElementById('country').value;
+    //get the zip code value
+    const zip = document.getElementById('zip').value;
+    //get the value for feelings
+    const feeling = document.getElementById('feelings').value;
+    //validation
+    if (!countryCode || !zip || !feeling) {
+        alert("please check the input data");
+        return;
+    }
+
+    getWeatherData(zip, countryCode)
+        .then(weatherData => postWeatherEntry(POST_ENTRY_ENDPOINT, weatherData, feeling))
+        .catch(error => {
+            console.log(error);
+            updateUIWithAPIError();
+        });
+}
 
 /* Function to POST data */
+async function postWeatherEntry(url = '', weatherData, userReponse) {
+    if (weatherData && weatherData.list && weatherData.list.length > 0) {
+        const todaysWeather = weatherData.list[0];
+        console.log(todaysWeather);
+        const payload = {
+            date: todaysWeather.dt,
+            temperature: todaysWeather.main.temp,
+            feeling: userReponse,
+            city: weatherData.city.name,
+            country: weatherData.city.country
+        };
 
+        try {
+            await fetch(url, {
+                method: 'POST',
+                mode: 'cors',
+                cache: 'no-cache',
+                credentials: 'same-origin',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                referrerPolicy: 'no-referrer',
+                body: JSON.stringify(payload)
+            });
+
+            await getWeatherEntry(GET_ENTRY_ENDPOINT)
+        }
+        catch (error) {
+            console.log(error);
+            updateUIWithAPIError();
+        }
+    }
+}
 
 /* Function to GET Project Data */
+async function getWeatherEntry(url = '') {
+    const data = await fetch(url)
+        .then(response => response.json());
+
+    updateUI(data);
+}
+
+/* Function to update UI with most recent entry of weather journal */
+function updateUI(mostRecentEntry) {
+    let holderDiv = document.querySelector('.holder');
+    let entryDiv = document.createElement('div');
+    entryDiv.className = 'entry';
+
+    let titleDiv = document.createElement('div');
+    titleDiv.className = 'title';
+    titleDiv.innerHTML = `Most Recent Entry for ${mostRecentEntry.city}, ${mostRecentEntry.country}`;
+
+    let entryHolder = document.createElement('div');
+    entryHolder.setAttribute('id', 'entryholder');
+
+    let dateDiv = document.createElement('div');
+    dateDiv.setAttribute('id', 'date');
+    let tempDiv = document.createElement('div');
+    tempDiv.setAttribute('id', 'temp');
+
+    entryHolder.appendChild(dateDiv);
+    entryHolder.appendChild(tempDiv);
+
+    let contentDiv = document.createElement('div');
+    contentDiv.setAttribute('id', 'content');
+
+    entryDiv.appendChild(titleDiv);
+    entryDiv.appendChild(entryHolder);
+    entryDiv.appendChild(contentDiv);
+    holderDiv.appendChild(entryDiv);
+
+    document.getElementById('date').innerHTML = mostRecentEntry.date;
+    document.getElementById('temp').innerHTML = `${mostRecentEntry.temperature}<span>&#8451;</span>`;
+    document.getElementById('content').innerHTML = mostRecentEntry.feeling;
+}
+
+/* Function to reset UI */
+function resetUI() {
+    const entryDiv = document.querySelector('.entry');
+    if (entryDiv) {
+        entryDiv.remove();
+    }
+}
+
+/* Function to update UI with an error message */
+function updateUIWithAPIError(message) {
+    let holderDiv = document.querySelector('.holder');
+    let entryDiv = document.createElement('div');
+    entryDiv.className = 'entry';
+
+    let titleDiv = document.createElement('div');
+    titleDiv.className = 'title';
+    titleDiv.innerHTML = message ? message : 'Sorry something went wrong!';
+
+    entryDiv.appendChild(titleDiv);
+    holderDiv.appendChild(entryDiv);
+}
+
+resetUI();
+
